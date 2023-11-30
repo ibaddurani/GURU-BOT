6e4d04d2b69cfc0ae5d79f122b6e839aa149c028
ibaddurani <152482817+ibaddurani@users.noreply.github.com>
2023-11-30 14:23:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923439019639', 'AYYAN', true],
+  ['034300000000', 'IBAD', true], 
   [''] 
 ] //Number of owners
 
@@ -52,11 +52,11 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'AYAN┃ᴮᴼᵀ' 
+global.author = '@pakhtoon' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/lala.inside' 
+global.dygp = 'https://chat.whatsapp.com/HziER5bpJOZ47fc0S60EGD'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
